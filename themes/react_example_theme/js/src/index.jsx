45b33c556c2882c2853f47327f73ca56350e995a
pixelstorm <dan@pixelstorm.com.au>
2024-05-11 15:21:17 +1000

    add react to index.jsx in theme dir

---
+++ themes/react_example_theme/js/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// # Example 1: Simple "Hello, World" code
+ReactDOM.render(
+  <h1>Hello there - world!</h1>,
+  document.getElementById('react-app')
+);
+
