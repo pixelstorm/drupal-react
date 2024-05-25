3a702de2b6738af9ed55d1e9ea3835447ebeb285
pixelstorm <dan@pixelstorm.com.au>
2024-05-25 17:24:11 +1000

    use fetch api inside react app to pull usage data from drupal.org api

--- themes/react_example_theme/js/src/index.jsx
+++ themes/react_example_theme/js/src/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-// # Example 1: Simple "Hello, World" code
-ReactDOM.render(
-  <h1>Hello there - world!</h1>,
-  document.getElementById('react-app')
+/* Import Components */
+import DrupalProjectStats from './components/DrupalProjectStats';
+
+const Main = () => (
+  <DrupalProjectStats projectName="drupal" />
 );
+
+// Get the container for your app.
+const container = document.getElementById('react-app');
+
+// Check if the container exists to avoid null errors.
+if (container) {
+  // Create a root.
+  const root = ReactDOM.createRoot(container);
+
+  // Render the Main component.
+  root.render(<Main />);
+} else {
+  console.error('Failed to find the root element');
+}
+
 
